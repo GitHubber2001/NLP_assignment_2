1811b8e36d2d0d09f76b43f722b5279401332e67
GitHubber2001 <kevin-kuipers@home.nl>
2026-03-05 13:05:48 +0100

    added timer decorator in Timer class

--- timer.py
+++ timer.py
@@ -11,9 +11,7 @@ class Timer:
         """Initiliaze Timer"""
 
         if not isinstance(name, str):
-            raise TypeError(
-                f"Name argument must be a string ({name} was given)"
-            )
+            raise TypeError(f"Name argument must be a string ({name} was given)")
 
         self._name = name
         self._start_time = None
@@ -48,24 +46,25 @@ class Timer:
             print(f"Time elapsed during '{self._name}': {elapsed_time:.2f}")
 
     @staticmethod
-    def TimeFunction(name: str, func: Callable) -> None:
-        """Time the duration of a function"""
+    def Time(name: str) -> Callable:
+        """Decorator that time the duration of a function"""
 
         if not isinstance(name, str):
-            raise TypeError(
-                f"Name argument must be a string ({name} was given)"
-            )
+            raise TypeError(f"Name argument must be a string ({name} was given)")
 
-        if not isinstance(func, Callable):
-            raise TypeError("paramter func must be of type Callable")
+        def TimeFunction(func: Callable):
+            if not isinstance(func, Callable):
+                raise TypeError("parameter func must be of type Callable")
+    
+            start_time = perf_counter()
+            results = func()
+            end_time = perf_counter()
 
-        start_time = perf_counter()
-        results = func()
-        end_time = perf_counter()
+            elapsed_time = end_time - start_time
 
-        elapsed_time = end_time - start_time
+            if DEBUG_ENABLED:
+                print(f"Time elapsed during '{name}': {elapsed_time:.2f}")
 
-        if DEBUG_ENABLED:
-            print(f"Time elapsed during '{name}': {elapsed_time:.2f}")
+            return results
 
-        return results
+        return TimeFunction
